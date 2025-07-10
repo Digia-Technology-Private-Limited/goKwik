543cfe879d3e1397b8be54198afe4c130630cd76
Karan Bhanushali <karan.bhanushali@gokwik.co>
2025-07-10 12:38:16 +0530

    MoEngage and WebEengage events tracking config.

---
+++ example/android/app/src/main/kotlin/com/example/example/MainApplication.kt
@@ -0,0 +1,35 @@
+package com.example.example
+
+import android.app.Application
+import io.flutter.app.FlutterApplication;
+import com.webengage.webengage_plugin.WebengageInitializer;
+import com.webengage.sdk.android.WebEngageConfig;
+import com.webengage.sdk.android.WebEngage;
+import com.webengage.sdk.android.LocationTrackingStrategy;
+
+import com.moengage.flutter.MoEInitializer
+import com.moengage.core.DataCenter
+import com.moengage.core.LogLevel
+import com.moengage.core.MoEngage
+import com.moengage.core.config.LogConfig
+import com.moengage.core.config.MoEngageEnvironmentConfig
+import com.moengage.core.model.environment.MoEngageEnvironment
+
+class MainApplication : FlutterApplication() {
+
+override fun onCreate() {
+        super.onCreate()
+        val webEngageConfig = WebEngageConfig.Builder()
+            .setWebEngageKey("~47b64dd1")
+            .setAutoGCMRegistrationFlag(false)
+            .setLocationTrackingStrategy(LocationTrackingStrategy.ACCURACY_BEST)
+            .setDebugMode(true) // only in development mode
+            .build()
+        WebengageInitializer.initialize(this as Application?, webEngageConfig)
+
+        val moEngage: MoEngage.Builder = MoEngage.Builder(this as Application, "MOE_ID",DataCenter.DATA_CENTER_3)
+            .configureLogs(LogConfig(LogLevel.VERBOSE, true))
+            .configureMoEngageEnvironment(MoEngageEnvironmentConfig(MoEngageEnvironment.LIVE))
+        MoEInitializer.initialiseDefaultInstance(applicationContext, moEngage)
+    }
+}
